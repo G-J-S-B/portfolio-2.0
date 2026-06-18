46966178ecb7f0f5dabc711f363217d6456b0303
G-J-S-B <gerrard.barrows@gmail.com>
2026-06-18 09:40:31 +0500

    refined Header

--- portfolio-2.0/src/components/Header/Header.jsx
+++ portfolio-2.0/src/components/Header/Header.jsx
@@ -1,20 +1,47 @@
 import './Header.css'
+import navItems from '../../data/nav-items.json'
+import { useState } from 'react'
+
 
 function Header(){
+    const [isOpen, setIsOpen] = useState(false)
+
+
+    function handleMenuClick(){
+        setIsOpen(!isOpen)
+    }
+
+
     return(
         <div className="container">
-            <nav>
+            <header>
                 <a href="#" className="nav-icon">
                     <img src="/images/placeholder.png" alt="" />
                     <span>GJSB</span>
                 </a>                    
-                <div className='nav-btn'>
-                    <span>XXXX</span>
+                <a onClick={handleMenuClick} className='custom-nav-btn'>
+                    <span className='circle'></span>
+                    <span className='circle'></span>
+                    <span className='circle'></span>
+                    <span className='circle'></span>
+                </a>
+                <a className="nav-resume-btn" href="">Resume</a>
+            </header>
+            {isOpen && (
+            <nav>
+                <div className="nav-items">
+                    {navItems.map((item) => (
+                       <a href={item.href} key={item.label}>
+                        {item.label}
+                        </a> 
+                    )) 
+                    } 
                 </div>
-                    <a className="nav-resume-btn" href="">Resume</a>
             </nav>
+            )}
         </div>
+        
+        
     )
 }
-
 export default Header
